e2c0eca52099c73b0defa65e9f0139fb35930420
zhizhin.ik <zhizhin.ik@phystech.edu>
2020-11-11 01:16:09 +0300

    Подправил решение 2 задачи 8 лабораторной работы

--- laboratory_8/task_8.2.py
+++ laboratory_8/task_8.2.py
@@ -1,15 +1,10 @@
 def fib(x):
     a=1
     b=1
-    if x<3:
-        while x>0:
-            yield a
-            x-=1
-    else:
-        while x>0:
-            yield a
-            a,b =b, a+b
-            x-=1
+    while x>0:
+        yield a
+        a,b =b, a+b
+        x-=1
 
 n=int(input())
 for i in fib(n):
